f21f2b88010b5273727daa9f1cb4c027ad3bfa96
AdolfoZepeda <adolfitozepeda@gmail.com>
2025-02-13 19:47:23 -0700

    another commit

--- script/script.js
+++ script/script.js
@@ -1,37 +1,4 @@
 
-document
-  .getElementById("change-theme-website")
-  .addEventListener("click", () => {
-    const i = document.querySelector("#change-theme-website i");
-    const root = document.documentElement;
-
-    function DarkMode() {
-      root.style.setProperty("--background", "#141414");
-      root.style.setProperty("--hover", "#161616");
-      root.style.setProperty("--color", "white");
-      root.style.setProperty("--border", "solid 1px rgb(26, 26, 26)");
-      root.style.setProperty("--shadow", "0px 3px 10px #272727");
-      root.style.setProperty("--box-shadow", "2px 2px 4px 0px #eee");
-      bgFixed.style.background = '#141414';
-    }
-
-    function LightMode() {
-      root.style.setProperty("--background", "#fafafa");
-      root.style.setProperty("--hover", "#f5f5f5");
-      root.style.setProperty("--color", "black");
-      root.style.setProperty("--border", "solid 1px #141414");
-      root.style.setProperty("--box-shadow", "2px 2px 4px 0px #141414");
-      bgFixed.style.background = ' rgb(196, 196, 196)';
-
-    }
-
-    i.classList.toggle("fa-moon");
-    i.classList.toggle("fa-sun");
-
-    i.className.replace("fa-solid ", "") === "fa-sun"
-      ? LightMode()
-      : DarkMode();
-  });
 
 //vertical observer
 
@@ -93,7 +60,6 @@ const observerUp = new IntersectionObserver((entries) => {
 const hiddenElementsUp = document.querySelectorAll('.hiddenUp');
 hiddenElementsUp.forEach((el) => observerUp.observe(el));
 
-
 //navbar responsive
 var openBtn = document.getElementById('barsBtn');
 var closeBtn = document.getElementById('xBtn');
@@ -138,7 +104,7 @@ function closeNav() {
 function openNav() {
   let viewportWidth = window.innerWidth;
 
-  if (viewportWidth < 780) {
+  if (viewportWidth < 1024) {
     header.classList.add('openNav');
     header.classList.remove('closeNav');
 
